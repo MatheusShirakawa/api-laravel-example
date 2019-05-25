1173b455f016aaacc09b5f8c861e8a1668e7b522
MatheusShirakawa <matheus_shirakawa@outlook.com>
2019-05-25 10:54:33 -0300

    first commit

---
+++ app/Http/Middleware/AuthKey.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AuthKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {   
+
+        $token = $request->header('APP_KEY');
+        
+        if($token != '1234567'){
+            return response()->json(['message','App key not authorized'],401);
+        }
+
+        return $next($request);
+    }
+}
